1e2862627ddb18b2d23f3f6544c019899506b968
Jan Bols <jan.bols@axians.com>
2018-12-19 21:06:56 +0100

    day13 - victory!

--- day13/Main.hs
+++ day13/Main.hs
@@ -4,6 +4,7 @@ import           Data.List
 import           Data.Matrix
 import           Data.Maybe
 import           Data.Ord
+import           Data.String
 import           Debug.Trace
 import           Utils
 
@@ -13,20 +14,28 @@ main = do
   input <- readFile "./day13/input.txt"
   if ptNr == 1
     then print $ pt1 $ lines input
-    else print $ pt2 input
+    else print $ pt2 $ lines input
 
-pt1 :: [String] -> ([Cart], Int)
-pt1 s = (firstCrash, crashTick)
+pt1 :: [String] -> P
+pt1 s = (y-1, x-1)
   where
     paddedLines = linesPadRight '.' s
     tracks = toTracks paddedLines
     carts = toCarts paddedLines
     cartIters = iterCarts tracks carts
-    firstCrash = fromJust $ find hasCrash cartIters
-    crashTick = fromJust $ findIndex hasCrash cartIters
+    firstCrash = fromJust $ find (isJust . crashedCarts) cartIters
+    (x,y) = p $ head $ fromJust $ crashedCarts firstCrash
 
-pt2 :: String -> Int
-pt2 s = undefined
+pt2 :: [String] -> P
+pt2 s = (y - 1, x - 1)
+  where
+    paddedLines = linesPadRight '.' s
+    tracks = toTracks paddedLines
+    carts = toCarts paddedLines
+    cartIters = iterCartsAndRemove tracks carts
+    onlyCarLeft = dropWhile (\cs -> length cs > 1) cartIters !! 1
+    (x, y) = p $ head onlyCarLeft
+
 
 type P = (Int, Int)
 
@@ -43,19 +52,22 @@ data Turn
   deriving (Show)
 
 data Cart = Cart
-  { p    :: P
+  { id   :: Int
+  , p    :: P
   , dir  :: Char
-  , turn :: [Turn]
-  }
-
-instance Show Cart where
-  show (Cart p dir (t:ts)) = "Cart" ++ show p ++ show dir ++ show t
+  , turn :: Turn
+  } deriving (Show)
 
 instance Eq Cart where
-  (Cart p1 _ _) == (Cart p2 _ _) = p1 == p2
+  (Cart id1 _ _ _) == (Cart id2 _ _ _) = id1 == id2
 
 instance Ord Cart where
   compare = comparing p
+
+nextTurn:: Turn-> Turn
+nextTurn L = S
+nextTurn S = R
+nextTurn R = L
 
 testFile = readFile "./day13/test.txt"
 
@@ -83,20 +95,26 @@ parseTrack c =
     c   -> Track c
 
 parseCart :: P -> Char -> Maybe Cart
-parseCart p c =
+parseCart p@(x, y) c =
   case c of
-    '^' -> Just (Cart p '^' $ cycle [L, S, R])
-    'v' -> Just (Cart p 'v' $ cycle [L, S, R])
-    '<' -> Just (Cart p '<' $ cycle [L, S, R])
-    '>' -> Just (Cart p '>' $ cycle [L, S, R])
+    '^' -> Just (Cart 0 p '^' L)
+    'v' -> Just (Cart 0 p 'v' L)
+    '<' -> Just (Cart 0 p '<' L)
+    '>' -> Just (Cart 0 p '>' L)
     c   -> Nothing
 
 toTracks s = parseTrack <$> fromLists (linesPadRight ' ' s) :: Matrix Track
 
-toCarts s = sort $ fmap fromJust $ filter isJust $ toList $ mapPos parseCart $ fromLists (linesPadRight ' ' s) :: [Cart]
+toCarts s = sort cards :: [Cart]
+  where
+    anonymousCards = fmap fromJust $ filter isJust $ toList $ mapPos parseCart $ fromLists (linesPadRight ' ' s)
+    cards = (\(id, Cart _ p d t) -> Cart id p d t) <$> zip [1 ..] anonymousCards
+
+
+
 
 moveCart :: Matrix Track -> Cart -> Cart
-moveCart tracks c@(Cart (x, y) dir turn) =
+moveCart tracks c@(Cart _ _ dir _) =
   case dir of
     '^' -> moveUp tracks c
     '>' -> moveRight tracks c
@@ -104,81 +122,95 @@ moveCart tracks c@(Cart (x, y) dir turn) =
     '<' -> moveLeft tracks c
     _   -> c
 
-moveUp tracks c@(Cart (x, y) dir turn) =
+moveUp tracks c@(Cart id (x, y) dir turn) =
   case targetTrack of
-    Track '|'  -> Cart targetPoint '^' turn
-    Track '\\' -> Cart targetPoint '<' turn
-    Track '/'  -> Cart targetPoint '>' turn
-    Track '+'  -> Cart targetPoint (findTurn $ head turn) $ tail turn
-    _ -> error("Cannot match " ++ show c ++ " against " ++ show targetTrack)
+    Track '|' -> Cart id targetPoint '^' turn
+    Track '\\' -> Cart id targetPoint '<' turn
+    Track '/' -> Cart id targetPoint '>' turn
+    Track '+' -> Cart id targetPoint (findDir turn) $ nextTurn turn
+    _ -> error ("Cannot match " ++ show c ++ " against " ++ show targetTrack)
   where
     targetPoint = (x - 1, y)
     targetTrack = tracks ! targetPoint
-    findTurn L = '<'
-    findTurn S = '^'
-    findTurn R = '>'
+    findDir L = '<'
+    findDir S = '^'
+    findDir R = '>'
 
-moveDown tracks c@(Cart (x, y) dir turn) =
+moveDown tracks c@(Cart id (x, y) dir turn) =
   case targetTrack of
-    Track '|'  -> Cart targetPoint 'v' turn
-    Track '\\' -> Cart targetPoint '>' turn
-    Track '/'  -> Cart targetPoint '<' turn
-    Track '+'  -> Cart targetPoint (findTurn $ head turn) $ tail turn
-    _ -> error("Cannot match " ++ show c ++ " against " ++ show targetTrack)
+    Track '|' -> Cart id targetPoint 'v' turn
+    Track '\\' -> Cart id targetPoint '>' turn
+    Track '/' -> Cart id targetPoint '<' turn
+    Track '+' -> Cart id targetPoint (findDir turn) $ nextTurn turn
+    _ -> error ("Cannot match " ++ show c ++ " against " ++ show targetTrack)
   where
     targetPoint = (x + 1, y)
     targetTrack = tracks ! targetPoint
-    findTurn L = '>'
-    findTurn S = 'v'
-    findTurn R = '<'
+    findDir L = '>'
+    findDir S = 'v'
+    findDir R = '<'
 
-moveLeft tracks c@(Cart (x, y) dir turn) =
+moveLeft tracks c@(Cart id (x, y) dir turn) =
   case targetTrack of
-    Track '-'  -> Cart targetPoint '<' turn
-    Track '\\' -> Cart targetPoint '^' turn
-    Track '/'  -> Cart targetPoint 'v' turn
-    Track '+'  -> Cart targetPoint (findTurn $ head turn) $ tail turn
-    _ -> error("Cannot match " ++ show c ++ " against " ++ show targetTrack)
+    Track '-' -> Cart id targetPoint '<' turn
+    Track '\\' -> Cart id targetPoint '^' turn
+    Track '/' -> Cart id targetPoint 'v' turn
+    Track '+' -> Cart id targetPoint (findDir turn) $ nextTurn turn
+    _ -> error ("Cannot match " ++ show c ++ " against " ++ show targetTrack)
   where
-    targetPoint = (x, y-1)
+    targetPoint = (x, y - 1)
     targetTrack = tracks ! targetPoint
-    findTurn L = 'v'
-    findTurn S = '<'
-    findTurn R = '^'
+    findDir L = 'v'
+    findDir S = '<'
+    findDir R = '^'
 
-moveRight tracks c@(Cart (x, y) dir turn) =
+moveRight tracks c@(Cart id (x, y) dir turn) =
   case targetTrack of
-    Track '-'  -> Cart targetPoint '>' turn
-    Track '\\' -> Cart targetPoint 'v' turn
-    Track '/'  -> Cart targetPoint '^' turn
-    Track '+'  -> Cart targetPoint (findTurn $ head turn) $ tail turn
-    _ -> error("Cannot match " ++ show c ++ " against " ++ show targetTrack)
+    Track '-' -> Cart id targetPoint '>' turn
+    Track '\\' -> Cart id targetPoint 'v' turn
+    Track '/' -> Cart id targetPoint '^' turn
+    Track '+' -> Cart id targetPoint (findDir turn) $ nextTurn turn
+    _ -> error ("Cannot match " ++ show c ++ " against " ++ show targetTrack)
   where
-    targetPoint = (x, y+1)
+    targetPoint = (x, y + 1)
     targetTrack = tracks ! targetPoint
-    findTurn L = '^'
-    findTurn S = '>'
-    findTurn R = 'v'
+    findDir L = '^'
+    findDir S = '>'
+    findDir R = 'v'
 
-moveOne :: Matrix Track -> ([Cart],[Cart]) -> ([Cart], [Cart])
-moveOne tracks ([], dones) = ([], dones)
-moveOne tracks ((td:tds), dones) = if any (isCrash done) tds then (tds, done:dones) else moveOne tracks (tds, (done:dones))
+tickOne :: Matrix Track -> [Cart] -> [Cart]
+tickOne tracks carts = Debug.Trace.trace (showIds (sort $ todos ++ dones)) (todos ++ dones)
   where
-    done = moveCart tracks td
+    go :: ([Cart], [Cart]) -> ([Cart], [Cart])
+    go ([], dones) = ([], dones)
+    go (td:tds, dones) =
+      if any (isCrash moved) tds || any (isCrash moved) dones
+        then Debug.Trace.trace ("crash!!" ++ show moved) (tds, moved : dones)
+        else go (tds, moved : dones)
+      where
+        moved = moveCart tracks td
+    (todos, dones) = go (sort carts, [])
 
-tickOne:: Matrix Track -> [Cart] -> [Cart]
-tickOne tracks carts = todos ++ dones
+showIds carts = unlines $ (\ (Cart id (x,y) dir t) -> show (y-1) ++ "," ++ show (x-1) ++ " " ++ show dir) <$> carts
+
+
+isCrash :: Cart -> Cart -> Bool
+isCrash (Cart _ p1 _ _) (Cart _ p2 _ _) = p1 == p2
+
+crashedCarts :: [Cart] -> Maybe [Cart]
+crashedCarts carts = find (\ cs -> length cs > 1) grouped
   where
-    sorted = sort carts
-    (todos, dones) = moveOne tracks (sorted, [])
+    grouped = groupBy (\c1 c2 -> p c1 == p c2) carts
+
+iterCarts :: Matrix Track -> [Cart] -> [[Cart]]
+iterCarts tracks = iterate (sort . tickOne tracks)
+
+removeCrashed:: [Cart] -> [Cart]
+removeCrashed carts = concat (filter (\cs -> length cs == 1) grouped)
+  where
+    grouped = groupBy (\c1 c2 -> p c1 == p c2) carts
 
 
+iterCartsAndRemove :: Matrix Track -> [Cart] -> [[Cart]]
+iterCartsAndRemove tracks = iterate (removeCrashed . sort . tickOne tracks)
 
-hasCrash:: [Cart] -> Bool
-hasCrash carts = or $ zipWith isCrash carts (tail carts)
-
-isCrash:: Cart -> Cart -> Bool
-isCrash (Cart p1 _ _) (Cart p2 _ _) = p1 == p2
-
-iterCarts:: Matrix Track -> [Cart] -> [[Cart] ]
-iterCarts tracks = iterate (tickOne tracks)
